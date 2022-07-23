2889f8300be552d5252bc32a5c47cbb0799de526
Collin Jones <collin.g.jones34@gmail.com>
2022-07-23 13:01:27 -0500

    had some help with this one

--- JavaScriptPractice/javascript-practice.js
+++ JavaScriptPractice/javascript-practice.js
@@ -23,7 +23,17 @@
 // getGrade(40, 64, 90)
 
 
-function removeEveryOther(arr){
-    return arr.filter((_, idx) => idx % 2 === 0);
+// function removeEveryOther(arr){
+//     return arr.filter((_, idx) => idx % 2 === 0);
+// }
+
+function sumArray(array)  {
+    if(array && array.length > 1) {
+        const sortedArray = array.sort((a,b) => a - b).slice(1, -1)
+        return sortedArray.reduce((acc, cur) => acc + cur, 0)
+    }
+
+    return 0
 }
+
 
