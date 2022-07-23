fd11df39ddb1d67e1f13c59ccae08e003cdfcd5f
Collin Jones <collin.g.jones34@gmail.com>
2022-07-23 12:45:36 -0500

    function works but not the way the test wants, just commiting then will commit fix

--- JavaScriptPractice/javascript-practice.js
+++ JavaScriptPractice/javascript-practice.js
@@ -5,19 +5,28 @@
 // }
 
 
-function getGrade (s1, s2, s3) {
-    let score = (s1 + s2 + s3) / 3;
+// function getGrade (s1, s2, s3) {
+//     let score = (s1 + s2 + s3) / 3;
+//
+//     if(score >= 90 && score <= 100){
+//         return "A";
+//     }else if(score >= 80 && score <= 90){
+//         return "B";
+//     }else if(score >= 70 && score <= 80){
+//         return "C";
+//     }else if(score >= 60 && score <= 70){
+//         return "D";
+//     }else{
+//         return "F";
+//     }
+// }
+// getGrade(40, 64, 90)
 
-    if(score >= 90 && score <= 100){
-        return "A";
-    }else if(score >= 80 && score <= 90){
-        return "B";
-    }else if(score >= 70 && score <= 80){
-        return "C";
-    }else if(score >= 60 && score <= 70){
-        return "D";
-    }else{
-        return "F";
+let arr = ['Tarkov', 'Is', 'Amazingly', 'a', 'Trash', 'Game']
+function removeEveryOther(arr){
+    for (let i = 0; i < arr.length; i++) {
+        arr.splice(i + 1);
     }
 }
-getGrade(40, 64, 90)
+removeEveryOther(arr)
+console.log(arr)
