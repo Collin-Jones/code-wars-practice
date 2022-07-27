84e6b78e2cd0217b671a25f07b9e291a0f7cab4c
colli <collin.g.jones34@gmail.com>
2022-07-27 14:38:01 -0500

    fixed the code, it didn't work but I was just missing a few things.

--- JavaScriptPractice/javascript-practice.js
+++ JavaScriptPractice/javascript-practice.js
@@ -22,38 +22,55 @@
 // }
 // getGrade(40, 64, 90)
 
-function highAndLow(nums){
-    let numbers = nums.split(' ');
-    let sorted = numbers.sort(function (a, b){
-        return Number(a) - Number(b);
-    });
-    return sorted [0] + " " + sorted[sorted.length - 1];
-}
-
-function highAndLow(numbers){
-    numbers = numbers.split(" ");
-    return Math.max(...numbers) +" "+ Math.min(...numbers);
-}
-
-function highAndLow(numbers){
-    numbers2=numbers.split(' ');
-    var highest =parseInt(numbers2[0]);
-    var lowest =parseInt(numbers2[0]);
-    if (numbers2.length==1) {
-        return numbers;
-    }
-    else {
-        for (i=0;i<numbers2.length;i++) {
-            if (parseInt(numbers2[i])>highest) {
-                highest = parseInt(numbers2[i]);
-            }
-            else if (parseInt(numbers2[i])<lowest) {
-                lowest = parseInt(numbers2[i]);
-            }
-        }
-    }
-    return(highest + " " + lowest);
-}
+// function highAndLow(nums){
+//     let numbers = nums.split(' ');
+//     let sorted = numbers.sort(function (a, b){
+//         return Number(a) - Number(b);
+//     });
+//     return sorted [0] + " " + sorted[sorted.length - 1];
+// }
+//
+// function highAndLow(numbers){
+//     numbers = numbers.split(" ");
+//     return Math.max(...numbers) +" "+ Math.min(...numbers);
+// }
+//
+// function highAndLow(numbers){
+//     numbers2=numbers.split(' ');
+//     var highest =parseInt(numbers2[0]);
+//     var lowest =parseInt(numbers2[0]);
+//     if (numbers2.length==1) {
+//         return numbers;
+//     }
+//     else {
+//         for (i=0;i<numbers2.length;i++) {
+//             if (parseInt(numbers2[i])>highest) {
+//                 highest = parseInt(numbers2[i]);
+//             }
+//             else if (parseInt(numbers2[i])<lowest) {
+//                 lowest = parseInt(numbers2[i]);
+//             }
+//         }
+//     }
+//     return(highest + " " + lowest);
+// }
+//
+// function highAndLow(numbers){
+//     num=numbers.split(' ');
+//     var max = parseInt(num[0]);
+//     var min = parseInt(num[0]);
+//     for (var i = 0; i <= num.length; i++) {
+//         if(parseInt(num[i]) > max){
+//             max = parseInt(num[i]);
+//         }
+//     }
+//     for (var i = 0; i <= num.length; i++) {
+//         if(parseInt(num[i]) < min){
+//             min = parseInt(num[i]);
+//         }
+//     }
+//     return (max + " " + min);
+// }
 
 // function removeEveryOther(arr){
 //     return arr.filter((_, idx) => idx % 2 === 0);
@@ -98,7 +115,8 @@ function highAndLow(numbers){
 //     return x <= 18.5 ? "Underweight" : x <= 25.0 ? "Normal" : x <= 30.0 ? "Overweight" : "Obese";
 // }
 
-const areaOrPerimeter = function(l , w) {
-    return l === w ? w * l : l * 2 + w * 2
-};
+// const areaOrPerimeter = function(l , w) {
+//     return l === w ? w * l : l * 2 + w * 2
+// };
+
 
