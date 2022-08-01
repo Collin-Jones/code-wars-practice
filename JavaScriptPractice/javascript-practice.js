8ad7a52522daa9ce26a3705c53130a7b03975653
colli <collin.g.jones34@gmail.com>
2022-08-01 12:19:44 -0500

    more code wars practice

--- JavaScriptPractice/javascript-practice.js
+++ JavaScriptPractice/javascript-practice.js
@@ -153,3 +153,18 @@ function isSquare(n){
 function rowSumOddNumbers(n) {
     return n * n * n;
 }
+
+function longest(s1, s2) {
+
+    let a = [... new Set(s1.split(''))];
+    let b = [... new Set(s2.split(''))];
+
+    for (let i = 0; i < b.length; i++) {
+        a.push(b[i]);
+    }
+
+    let initcombo = [... new Set(a)];
+    let combo = initcombo.sort().join('')
+
+    return combo;
+}
